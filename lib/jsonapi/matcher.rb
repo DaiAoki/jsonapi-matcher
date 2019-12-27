805f6e4ceb712c8095c6b8b8ca6c889012d9aad6
DaiAoki <a.dai.0814ap@gmail.com>
2019-12-27 13:59:31 +0900

    bundle gem jsonapi-matcher -t

---
+++ lib/jsonapi/matcher.rb
@@ -0,0 +1,8 @@
+require "jsonapi/matcher/version"
+
+module Jsonapi
+  module Matcher
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jsonapi/matcher/version.rb
@@ -0,0 +1,5 @@
+module Jsonapi
+  module Matcher
+    VERSION = "0.1.0"
+  end
+end
